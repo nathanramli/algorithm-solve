6ff791e8ecf2237971a5d6f9f8ed620012efede7
Nathan Ramli <hezkyanatanael84@gmail.com>
2021-01-15 14:38:47 +0700

    add snippets

---
+++ snippets/combinatorics.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int combinatorics(int n, int r)
+{
+    ll p = 1, k = 1;
+    if (n - r < r)
+        r = n - r;
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+            ll m = __gcd(p, k);
+            p /= m;
+            k /= m;
+            n--;
+            r--;
+        }
+    }
+    else
+        p = 1;
+    return p;
+}
